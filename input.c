f9edfa9ba4d6f37c5222e1f8e704bbc7464e6526
Andreithebotkiller <144950784+AndreicoderHacks@users.noreply.github.com>
2026-05-24 22:10:49 +0300

    Update input.c

--- input.c
+++ input.c
@@ -1,21 +1,26 @@
 #include "minicraft.h"
 
 static u8 padBuf[256] __attribute__((aligned(64)));
-static struct padButtonStatus buttons;
 
 void input_init(void) {
     padInit(0);
     padPortOpen(0, 0, padBuf);
+    // Asteapta 120 frame-uri ca pad-ul sa se initializeze
+    for (int i = 0; i < 120; i++) {
+        padGetState(0, 0);
+    }
 }
 
 void input_update(GameState *gs) {
     gs->padPrev = gs->padCurrent;
     gs->padCurrent = 0;
 
+    struct padButtonStatus buttons;
     int state = padGetState(0, 0);
     if (state == PAD_STATE_STABLE || state == PAD_STATE_FINDCTP1) {
-        padRead(0, 0, &buttons);
-        gs->padCurrent = ~((u32)buttons.btns);
+        if (padRead(0, 0, &buttons) != 0) {
+            gs->padCurrent = ~((u32)buttons.btns) & 0xFFFF;
+        }
     }
 }
 
