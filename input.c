e9873d325b3207ca8357240557518f12ad4473bc
Andreithebotkiller <144950784+AndreicoderHacks@users.noreply.github.com>
2026-05-24 22:01:40 +0300

    Update input.c

--- input.c
+++ input.c
@@ -1,27 +1,23 @@
-// input.c - PS2 controller input via libpad
 #include "minicraft.h"
 
-static u8 padBuf[2][256] __attribute__((aligned(64)));
+static u8 padBuf[256] __attribute__((aligned(64)));
 static struct padButtonStatus buttons;
 
 void input_init(void) {
     padInit(0);
-    padPortOpen(0, 0, padBuf[0]);
-    padPortOpen(1, 0, padBuf[1]);
-    // Nu astepta - lasa game loop sa se ocupe
+    padPortOpen(0, 0, padBuf);
 }
 
 void input_update(GameState *gs) {
     gs->padPrev = gs->padCurrent;
+    gs->padCurrent = 0;
 
     int state = padGetState(0, 0);
     if (state == PAD_STATE_STABLE || state == PAD_STATE_FINDCTP1) {
-        if (padRead(0, 0, &buttons) != 0) {
-            gs->padCurrent = ~buttons.btns;
-        }
+        padRead(0, 0, &buttons);
+        u32 raw = ((u32)buttons.btns[0] | ((u32)buttons.btns[1] << 8));
+        gs->padCurrent = ~raw;
     }
-    // Daca pad nu e gata, pastreaza butoanele vechi
-    // (nu reseta la 0 ca sa nu blocheze meniul)
 }
 
 int input_pressed(GameState *gs, u32 button) {
