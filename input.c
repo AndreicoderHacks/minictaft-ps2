7a8fbf146bf29b54ceb57da8d7b400025185801c
ASPL <144950784+AndreicoderHacks@users.noreply.github.com>
2026-05-25 22:44:44 +0300

    Add files via upload

--- input.c
+++ input.c
@@ -13,11 +13,20 @@ void input_init(void) {
 void input_update(GameState *gs) {
     gs->padPrev = gs->padCurrent;
     gs->padCurrent = 0;
+
     struct padButtonStatus buttons;
     int state = padGetState(0, 0);
     if (state == PAD_STATE_STABLE || state == PAD_STATE_FINDCTP1) {
         if (padRead(0, 0, &buttons) != 0) {
-            gs->padCurrent = ~((u32)buttons.btns) & 0xFFFF;
+            // buttons.btns: bit=0 inseamna apasat, bit=1 inseamna liber
+            // Cand nimic nu e apasat, btns = 0xFFFF
+            u16 btns = buttons.btns;
+            if (btns != 0xFFFF) {
+                // Inverseaza doar bitii care sunt 0 (apasati)
+                gs->padCurrent = (~btns) & 0xFFFF;
+            } else {
+                gs->padCurrent = 0;
+            }
         }
     }
 }
