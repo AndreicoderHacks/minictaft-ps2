b4734f529b332b50e4b54cbcb9f5e38d94ad056d
ASPL <144950784+AndreicoderHacks@users.noreply.github.com>
2026-05-25 21:21:41 +0300

    Update minicraft.h

--- minicraft.h
+++ minicraft.h
@@ -19,10 +19,10 @@
 #define PAD_L3          0x0002
 #define PAD_R3          0x0004
 #define PAD_START       0x0008
-#define PAD_UP          0x0010
-#define PAD_RIGHT       0x0020
-#define PAD_DOWN        0x0040
-#define PAD_LEFT        0x0080
+#define PAD_UP          0x0800
+#define PAD_RIGHT       0x0400
+#define PAD_DOWN        0x0200
+#define PAD_LEFT        0x0100
 #define PAD_L2          0x0100
 #define PAD_R2          0x0200
 #define PAD_L1          0x0400
